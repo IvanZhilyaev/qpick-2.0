d65196ce796ba2112dc4e2eba259c0b0ce449a29
Ivan <ivan_zhilyaev@icloud.com>
2023-01-13 14:53:45 +0300

    done

--- js/script.js
+++ js/script.js
@@ -1,8 +1,29 @@
 const menuBtn = document.querySelector(".menu-btn");
 const menuForBtn = document.querySelector(".menu-for-btn");
-
 menuBtn.onclick = () => {
     menuForBtn.classList.toggle('active');
+}
+
+
+const menuBtn3 = document.querySelector(".menu-btn3");
+const menuForBtn2 = document.querySelector(".menu-for-btn2");
+const anotherBar = document.querySelector(".another-bar");
+const casesDiv = document.querySelector(".cases-section");
+const favSec = document.querySelector(".favorite-section");
+menuBtn3.onclick = () => {
+    menuForBtn2.classList.toggle('active');
+    anotherBar.classList.toggle('active');
+    casesDiv.classList.toggle('active');
+    favSec.classList.toggle('active');
+}
+
+
+
+
+const secMenu = document.querySelector(".adapt-menu-bar");
+const menuBtn2 = document.querySelector(".menu-btn2");
+menuBtn2.onclick = () => {
+    secMenu.classList.toggle('active');
 }
 
 //cart
